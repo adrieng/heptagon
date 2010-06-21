12251f960ed6d583b703b2ab851c8044f57985aa
Léonard Gérard <leonard.gerard@ens.fr>
2010-06-21 12:11:06 +0200

    Add lib/pervasives.epi, tests, tools

---
+++ tools/hec-c.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# This small helper scripts automates the Heptagon -> C translation.
+
+if [ $# -lt 1 ]
+then
+  echo Usage: $0 file.ept
+  exit 1
+fi
+
+compile=1
+
+if [ $# -gt 2 ];
+then
+  nocompile=0
+fi
+
+F=$1
+REP=`basename $F .ept`_c
+shift
+
+# Compile source file to VHDL, flattening node calls
+if [ $compile -eq 1 ]; then
+  hec.byte $@ -s main -target c $F || exit 1
+fi
+
+# Compile it with GCC
+cc $REP/*.c -o `basename $F .ept` || exit 1
