d0b222b00307ada97bd9dd5d145102cd01ebab9b
Adrien Guatto <adrien.guatto@laposte.net>
2010-06-30 16:59:51 +0200

    Working Menhir parser with --table

--- compiler/myocamlbuild.ml
+++ compiler/myocamlbuild.ml
@@ -6,8 +6,18 @@ let df = function
       (* Tell ocamlbuild about the camlp4 library. *)
       ocaml_lib ~extern:true ~dir:"+camlp4" "camlp4";
 
+      (* Tell ocamlbuild about Menhir library (needed by --table). *)
+      ocaml_lib ~extern:true ~dir:"+menhirLib" "menhirLib";
+
+      (* Menhir does not come with menhirLib.cmxa so we have to manually by-pass
+         OCamlbuild's built-in logic and add the needed menhirLib.cmxa. *)
+      flag ["link"; "link_menhirLib"] (S [A "-I"; A "+menhirLib";
+                                          A "menhirLib.cmx"]);
+
+
       (* Add preproc.cmo to the ocaml pre-processor when use_preproc is set *)
       flag ["ocaml"; "pp"; "use_preproc"] (A "preproc.cmo");
+
       (* Running ocamldep on ocaml code that is tagged with use_preproc will
          require the cmo.  Note that you only need this declaration when the
          syntax extension is part of the sources to be compiled with
@@ -17,7 +27,8 @@ let df = function
       (* LablGTK use for graphical simulator *)
       ocaml_lib ~extern:true ~dir:"+lablgtk2" "lablgtk";
 
-      flag ["ocaml"; "parser" ; "menhir" ; "use_menhir"] (S[A"--explain"]);
+      flag ["ocaml"; "parser" ; "menhir" ; "use_menhir"] (S[A"--explain";
+                                                            A"--table"]);
 
   | _ -> ()
 
