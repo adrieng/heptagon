d44cd3571b67143cfdb3140fc6e8c4bcf0c35c26
Léonard Gérard <leonard.gerard@ens.fr>
2010-07-01 19:35:39 +0200

    enforce_style fix.

--- tools/enforce_style.sh
+++ tools/enforce_style.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-find \( -iname "*.ml" -or -iname "*.mli" \) -exec perl -pi -e 's/( |\t)+$//g' {} \;
-find \( -iname "*.ml" -or -iname "*.mli" \) -exec perl -pi -e 's/\t/  /g' {} \;
+find . \( -iname "*.ml" -or -iname "*.mli" \) -exec perl -pi -e 's/( |\t)+$//g' {} \;
+find . \( -iname "*.ml" -or -iname "*.mli" \) -exec perl -pi -e 's/\t/  /g' {} \;
