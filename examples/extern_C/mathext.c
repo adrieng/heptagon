4e5617588c077f9052e117e9e4e6d0bd18053536
Cédric Pasteur <cedric.pasteur@m4x.org>
2010-07-27 11:32:58 +0200

    Updated the example

--- examples/extern_C/mathext.c
+++ examples/extern_C/mathext.c
@@ -1,7 +1,21 @@
 #include <math.h>
 #include "mathext.h"
 
-void mycos(float a, mycos_out *out)
+void mycos_step(float a, mycos_out *out)
 {
   out->o = cos(a);
+}
+
+void st_cos_reset(st_cos_mem *self)
+{
+  self->i = 0;
+  for(int j = 0; j < 100; ++j)
+    self->mem[j] = 0.0;
+}
+
+void st_cos_step(float a, st_cos_out *out, st_cos_mem *self)
+{
+  out->o = self->mem[self->i];
+  self->i = (self->i++) % 100;
+  self->mem[self->i] = cos(a);
 }
