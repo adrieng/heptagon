ac8dce35dc6dead3e4d2f62c9af11e0168358840
Cédric Pasteur <cedric.pasteur@m4x.org>
2010-07-27 11:12:33 +0200

    Added an example of how to import external functions

---
+++ examples/extern_C/mathext.c
@@ -0,0 +1,7 @@
+#include <math.h>
+#include "mathext.h"
+
+void mycos(float a, mycos_out *out)
+{
+  out->o = cos(a);
+}
---
+++ examples/extern_C/mathext.h
@@ -0,0 +1,8 @@
+#ifdef MATHEXT_H
+#define MATHEXT_H
+
+struct mycos_out {
+  float o;
+};
+
+void mycos(float a, mycos_out *o);
