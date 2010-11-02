c6777389030321f20a66c1beee60d94b4c7fad2a
Léonard Gérard <leonard.gerard@ens.fr>
2010-11-02 11:31:59 +0100

    enforce_style.sh now deals with parsing files.

--- tools/enforce_style.sh
+++ tools/enforce_style.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find . \! -path "*_build*" -and \( -iname "*.ml" -or -iname "*.mli" \) -exec perl -pi -e 's/( |\t)+$//gi; s/\t/  /g' {} \;
+find . \! -path "*_build*" -and \( -iname "*.ml" -or -iname "*.mli" -or -iname "*.mly" -or -iname "*.mll" \) -exec perl -pi -e 's/( |\t)+$//gi; s/\t/  /g' {} \;
