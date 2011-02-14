979a6cfbd549765f5858c83d3446f2a3f2857075
Léonard Gérard <leonard.gerard@ens.fr>
2011-02-14 15:22:13 +0100

    Do stuff in pervasives.

--- lib/java/jeptagon/Pervasives.java
+++ lib/java/jeptagon/Pervasives.java
@@ -95,4 +95,14 @@ public class Pervasives {
     		c6 = v6;
     	}
     }
+
+    public static int do_stuff(int coeff) {
+        int x = 13;
+        for (int i = 0; i < coeff; i++) {
+            for (int j = 0; j < 1000000; j++) {
+                x = (x + j) % (x + j/x) + 13;
+            }
+        }
+        return x;
+    }
 }
