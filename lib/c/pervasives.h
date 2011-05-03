0256b571f7fd30c07cf980df82759b669ea8c33c
Cédric Pasteur <cedric.pasteur@ens.fr>
2011-05-03 13:35:13 +0200

    Added pervasives C header
    It only defines the between function for now

---
+++ lib/c/pervasives.h
@@ -0,0 +1,14 @@
+/* Pervasives module for the Decades compiler */
+
+#ifndef DECADES_PERVASIVES_H
+#define DECADES_PERVASIVES_H
+
+/* between(i, n) returns idx between 0 and n-1. */
+inline int between(int idx, int n)
+{
+  int o = (idx >= n) ? n-1 : (idx < 0 ? 0 : idx);
+  return o;
+}
+
+#endif
+
