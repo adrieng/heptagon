adrieng/heptagon

commit 6b86b86e9da031928b6bc69cea9b9d7d0c80dcc0

really fix normalize_mem.
in the old fix was missing the renaming, and there was wrong equations
order :
f () returns x
[here rename x by mem_x]
mem_x = fby ...
x = mem_x

so the simplest was :
f () returns out_x
var x;
[ nothing to do]
out_x = x
x = fby ...

pay attention to the order of equations since we are after the
scheduling..
probably it should be done before the scheduling anyway ?

compiler/minils/transformations/normalize_mem.ml
@@ -9,26 +9,30 @@ open Mls_mapfold
        o = mem_o
 *)
 
-let eq _ (v, eqs) eq = match eq.eq_lhs, eq.eq_rhs.e_desc with
+let rec vd_replace vd by_vd vd_l = match vd_l with
+  | [] -> []
+  | h::t -> if h.v_ident = vd.v_ident then by_vd::t else h::(vd_replace vd by_vd t)
+
+let eq _ (locals, eqs, outs) eq = match eq.eq_lhs, eq.eq_rhs.e_desc with
   | Evarpat x, Efby _ ->
-      if not (Mls_utils.vd_mem x v) then (* this memory is also an output *)
+      (try
+        let vd_out = Mls_utils.vd_find x outs in (* this memory is also an output *)
         let ty = eq.eq_rhs.e_ty in
-        let x_copy = Idents.gen_var "normalize_mem" ("mem_"^(Idents.name x)) in
-        let vd = mk_var_dec ~clock:eq.eq_rhs.e_ck x_copy ty in
-        let x_copy_exp = mk_exp ty (Eextvalue (mk_extvalue ~ty:ty (Wvar x_copy))) in
-        let eq_copy = { eq with eq_rhs = x_copy_exp } in
-        let eq = { eq with eq_lhs = Evarpat x_copy } in
-          eq, (vd::v, eq_copy::eq::eqs)
-      else
-        eq, (v, eq::eqs)
+        let x_copy = Idents.gen_var "normalize_mem" ("out_"^(Idents.name x)) in
+        let vd_x_copy = mk_var_dec ~clock:eq.eq_rhs.e_ck x_copy ty in
+        let exp_x = mk_exp ty (Eextvalue (mk_extvalue ~ty:ty (Wvar x))) in
+        let eq_copy = { eq with eq_lhs = Evarpat x_copy; eq_rhs = exp_x } in
+        eq, (vd_out::locals, eq::eq_copy::eqs, vd_replace vd_out vd_x_copy outs)
+      with Not_found -> (* this memory is not an output *)
+        eq, (locals, eq::eqs, outs))
   | _, _ ->
-      eq, (v, eq::eqs)
+      eq, (locals, eq::eqs, outs)
 
 let node funs acc nd =
-  let nd, (v, eqs) = Mls_mapfold.node_dec funs (nd.n_local, []) nd in
-    { nd with n_local = v; n_equs = List.rev eqs }, acc
+  let nd, (v, eqs, o) = Mls_mapfold.node_dec funs (nd.n_local, [], nd.n_output) nd in
+    { nd with n_local = v; n_equs = List.rev eqs; n_output = o }, acc
 
 let program p =
   let funs = { Mls_mapfold.defaults with eq = eq; node_dec = node } in
-  let p, _ = Mls_mapfold.program_it funs ([], []) p in
+  let p, _ = Mls_mapfold.program_it funs ([], [], []) p in
     p

compiler/minils/mls_utils.ml
@@ -36,7 +36,7 @@ let rec bounds_list ty =
 (** @return the [var_dec] object corresponding to the name [n]
     in a list of [var_dec]. *)
 let rec vd_find n = function
-  | [] -> Format.eprintf "Not found var %s@." (name n); raise Not_found
+  | [] -> (*Format.eprintf "Not found var %s@." (name n);*) raise Not_found
   | vd::l ->
       if vd.v_ident = n then vd else vd_find n l
 

commit 70787757aadfd6cdfb6808aede7689adb63a2d6a

Fixed normalization of fby
The incorrect behaviour was introduced in commit
6b86b86e9da031928b6bc69cea9b9d7d0c80dcc0.

We should have:
       o = v fby e
     becomes
       mem_o = v fby e;
       o = mem_o

The old way would break with:
 v = true fby false fby v
that would generate unschedulable code.

compiler/minils/main/mls_compiler.ml
@@ -40,7 +40,10 @@ let compile_program p =
   let p =
     pass "Automata minimization checks" true Tomato.tomato_checks p pp in
 *)
-  
+
+  (* Normalize memories*)
+  let p = pass "Normalize memories" true Normalize_mem.program p pp in
+
   (* Scheduling *)
   let p =
     if !Compiler_options.use_interf_scheduler then
@@ -48,9 +51,6 @@ let compile_program p =
     else
       pass "Scheduling" true Schedule.program p pp
   in
-
-  (* Normalize memories*)
-  let p = pass "Normalize memories" true Normalize_mem.program p pp in
 
   (* Memory allocation *)
   let p = pass "memory allocation" !do_mem_alloc Interference.program p pp in

compiler/minils/transformations/normalize_mem.ml
@@ -11,49 +11,34 @@ open Mls_mapfold
        o = mem_o
 *)
 
-let fix_vd_ad env vd ad =
-  if Env.mem vd.v_ident env then
-    let x_copy = Env.find vd.v_ident env in
-      { vd with v_ident = x_copy }, { ad with a_name = Some (Idents.name x_copy) }
-  else
-    vd, ad
-
-let eq _ (outputs, eqs, env) eq = match eq.eq_lhs, eq.eq_rhs.e_desc with
+let eq _ (outputs, v, eqs) eq = match eq.eq_lhs, eq.eq_rhs.e_desc with
   | Evarpat x, Efby _ ->
       if Mls_utils.vd_mem x outputs then
-        let ty = eq.eq_rhs.e_ty in
-        let lin = eq.eq_rhs.e_linearity in
-        let ck = eq.eq_rhs.e_base_ck in
-        let x_copy = Idents.gen_var "normalize_mem" ("out_"^(Idents.name x)) in
-        let exp_x =
-          mk_exp ck ~ck:ck ~ct:(Clocks.Ck ck) ty ~linearity:lin
-            (Eextvalue (mk_extvalue ~clock:ck ~ty:ty ~linearity:lin (Wvar x)))
-        in
-        let eq_copy = { eq with eq_lhs = Evarpat x_copy; eq_rhs = exp_x } in
-        let env = Env.add x x_copy env in
-          eq, (outputs, eq::eq_copy::eqs, env)
+        let vd = Mls_utils.vd_find x outputs in
+        let x_mem = Idents.gen_var "normalize_mem" ("mem_"^(Idents.name x)) in
+        let vd_mem = { vd with v_ident = x_mem } in
+        let exp_mem_x = mk_extvalue_exp vd.v_clock vd.v_type
+          ~clock:vd.v_clock ~linearity:vd.v_linearity (Wvar x_mem) in
+        (* mem_o = v fby e *)
+        let eq_copy = { eq with eq_lhs = Evarpat x_mem } in
+        (* o = mem_o *)
+        let eq = { eq with eq_rhs = exp_mem_x } in
+        eq, (outputs, vd_mem::v, eq::eq_copy::eqs)
       else (* this memory is not an output *)
-        eq, (outputs, eq::eqs, env)
+        eq, (outputs, v, eq::eqs)
   | _, _ ->
-      eq, (outputs, eq::eqs, env)
+      eq, (outputs, v, eq::eqs)
 
 (* Leave contract unchanged (no output defined in it) *)
 let contract funs acc c = c, acc
 
 let node funs acc nd =
-  let nd, (_, eqs, env) = Mls_mapfold.node_dec funs (nd.n_output, [], Env.empty) nd in
-  let v = Env.fold
-    (fun x _ acc -> (Mls_utils.vd_find x nd.n_output)::acc) env nd.n_local in
-  (* update the signature of the node *)
-  let f = Modules.find_value nd.n_name in
-  let o, sig_o = List.split (List.map2 (fix_vd_ad env) nd.n_output f.node_outputs) in
-  let f = { f with node_outputs = sig_o } in
-  Modules.replace_value nd.n_name f;
+  let nd, (_, v, eqs) = Mls_mapfold.node_dec funs (nd.n_output, nd.n_local, []) nd in
   (* return updated node *)
-  { nd with n_local = v; n_equs = List.rev eqs; n_output = o }, acc
+  { nd with n_local = v; n_equs = List.rev eqs }, acc
 
 let program p =
   let funs = { Mls_mapfold.defaults with
-		 eq = eq; node_dec = node; contract = contract } in
-  let p, _ = Mls_mapfold.program_it funs ([], [], Env.empty) p in
+    eq = eq; node_dec = node; contract = contract } in
+  let p, _ = Mls_mapfold.program_it funs ([], [], []) p in
     p