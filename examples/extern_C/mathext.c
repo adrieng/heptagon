f2c4507ea426e94fb0f57a3434bf926be64bc7c5
Gwenaël Delaval <gwenael.delaval@inria.fr>
2013-01-25 15:41:34 +0100

    Bug correction in extern_C example

--- examples/extern_C/mathext.c
+++ examples/extern_C/mathext.c
@@ -16,6 +16,6 @@ void st_cos_reset(st_cos_mem *self)
 void st_cos_step(float a, st_cos_out *out, st_cos_mem *self)
 {
   out->o = self->mem[self->i];
-  self->i = (self->i++) % 100;
+  self->i = (self->i+1) % 100;
   self->mem[self->i] = cos(a);
 }
