0a65ffc8e2d3f17d18de46566a52f09668be25dd
Gwenaël Delaval <gwenael.delaval@inria.fr>
2015-09-17 17:20:31 +0200

    Update of example Extern_C

--- examples/extern_C/mathext.h
+++ examples/extern_C/mathext.h
@@ -2,24 +2,24 @@
 #define MATHEXT_H
 
 /* Example of a combinatorial function */
-typedef struct mycos_out {
+typedef struct Mathext__mycos_out {
   float o;
-} mycos_out;
+} Mathext__mycos_out;
 
-void mycos_step(float a, mycos_out *o);
+void Mathext__mycos_step(float a, Mathext__mycos_out *o);
 
 /* Example of a statefull function. */
-typedef struct st_cos_out {
+typedef struct Mathext__st_cos_out {
   float o;
-} st_cos_out;
+} Mathext__st_cos_out;
 
-typedef struct st_cos_mem {
+typedef struct Mathext__st_cos_mem {
   int i;
   float mem[100];
-} st_cos_mem;
+} Mathext__st_cos_mem;
 
-void st_cos_reset(st_cos_mem *self);
-void st_cos_step(float a, st_cos_out *out, st_cos_mem *self);
+void Mathext__st_cos_reset(Mathext__st_cos_mem *self);
+void Mathext__st_cos_step(float a, Mathext__st_cos_out *out, Mathext__st_cos_mem *self);
 
 #endif
 
